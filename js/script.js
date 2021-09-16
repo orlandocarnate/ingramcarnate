f1253f99b5ed28a193e137ceb137ecc3e528785e
Orlando Carnate <orlando.carnate@gmail.com>
2021-09-16 09:35:53 -0500

    add fade-in animations using IntersectionObserver

--- js/script.js
+++ js/script.js
@@ -26,3 +26,26 @@ allLinks.forEach((link) => {
         }
     })
 })
+
+const observerOptions = {
+    root: null,
+    threshold: 0,
+    rootMargin: '0px 0px -50px 0px'
+}
+
+const observer = new IntersectionObserver( entries => {
+    entries.forEach( entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('in-view');
+            observer.unobserve(entry.target);
+        }
+    });
+}, observerOptions);
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    const sections = Array.from(document.getElementsByClassName('section-fade'));
+
+    for (let section of sections) {
+        observer.observe(section);
+    }
+});

ed7a62a29c28b32aba07a590dc92db00c4043d8a
Orlando Carnate <orlando.carnate@gmail.com>
2021-09-19 20:01:21 -0500

    finish mobile nav menu

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ allLinks.forEach((link) => {
     link.addEventListener('click', (e)=> {
         e.preventDefault();
         const href = link.getAttribute('href');
-
+        
         // scroll to top
         if (href === '#') {
             window.scrollTo({
@@ -25,7 +25,7 @@ allLinks.forEach((link) => {
                 behavior: "smooth"
             })
         }
-
+        
         if (href !== '#' && href.startsWith('#')) {
             const sectionElem = document.querySelector(href);
             sectionElem.scrollIntoView({
@@ -35,25 +35,50 @@ allLinks.forEach((link) => {
     })
 })
 
-const observerOptions = {
+// Sticky Navbar
+const stickyFade = {
+  root: null,
+  threshold: 0,
+  // stick Navbar when 80px within view
+  rootMargin: "-80px",
+};
+
+const sectionHeroElem = document.querySelector('.section-hero');
+
+const navObserver = new IntersectionObserver( entries => {
+    const entry = entries[0];
+    if (!entry.isIntersecting) {
+        document.body.classList.add('sticky');
+    }
+    if (entry.isIntersecting) {
+        document.body.classList.remove('sticky');
+    }
+}, stickyFade)
+
+navObserver.observe(sectionHeroElem);
+
+// Fade Intersection Observer
+
+const optionsFade = {
     root: null,
     threshold: 0,
+    // fade in when 50px within view
     rootMargin: '0px 0px -50px 0px'
 }
 
-const observer = new IntersectionObserver( entries => {
+const fadeInObserver = new IntersectionObserver( entries => {
     entries.forEach( entry => {
         if (entry.isIntersecting) {
             entry.target.classList.add('in-view');
-            observer.unobserve(entry.target);
+            fadeInObserver.unobserve(entry.target);
         }
     });
-}, observerOptions);
+}, optionsFade);
 
 window.addEventListener('DOMContentLoaded', (event) => {
     const sections = Array.from(document.getElementsByClassName('section-fade'));
 
     for (let section of sections) {
-        observer.observe(section);
+        fadeInObserver.observe(section);
     }
 });
